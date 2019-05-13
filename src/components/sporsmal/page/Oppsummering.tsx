611470b9bf40ef31896718a0a75f847cbad28d61
alexander-svendsen <alexander.svendsen@nav.no>
2019-05-13 10:45:06 +0200

    FO-2090: Nytt design på oppsummering siden

--- src/components/sporsmal/page/Oppsummering.tsx
+++ src/components/sporsmal/page/Oppsummering.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Normaltekst, Undertittel} from 'nav-frontend-typografi';
-import BekreftTekst from '../../dialog-lenke/BekreftTekst';
+import { AlertStripeSuksess } from 'nav-frontend-alertstriper';
 import {SporsmalProps} from "../SporsmalView";
 
 
@@ -9,17 +9,17 @@ function Oppsummering(props: SporsmalProps) {
         <>
             <div className="spm">
                 <Undertittel className="spm-row">
-                    Takk for svar
+                    Takk for tilbakemelding
                 </Undertittel>
                 <div className="spm-row">
-                    <BekreftTekst url={props.fallbackUrl}/>
+                    <AlertStripeSuksess>
+                        <Normaltekst>
+                            Svarene dine er er&nbsp;
+                            <a href={`${props.fallbackUrl}`}>delt med veilederen din</a>&nbsp;
+                            som nå vil kontakte deg i løpet av noen dager
+                        </Normaltekst>
+                    </AlertStripeSuksess>
                 </div>
-                <div className="spm-row">
-                    <Normaltekst >
-                        Veilederen din vil kontakte deg i løpet av noen dager for å avtale veien videre
-                    </Normaltekst>
-                </div>
-
             </div>
         </>
     );
