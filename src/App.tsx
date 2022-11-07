eddc651f576ab2073a7e6ccc7ca738695efd4fef
Lars Hartvigsen <lhartvik@gmail.com>
2022-11-07 11:06:04 +0100

    minside (#70)
    * Bytter fra ditt-nav til minside
    
    * bruker ny systemvariabel
    
    * Create gh-pages.yml
    ...

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.less';
 import Banner from './components/banner/Banner';
 import Sporsmal from './pages/Sporsmal';
-import { frontendLogger } from './components/util/frontendlogger';
+import {frontendLogger} from './components/util/frontendlogger';
 
 function App() {
     frontendLogger('forberede-moete.visit');
