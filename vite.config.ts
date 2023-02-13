6ca38601bd8acea0fd00b0cf3cba7657724d5243
sigurdgroneng <sigurd.groneng@nav.no>
2023-02-13 15:06:43 +0100

    Add dekoratoren

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,29 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-  },
-  test: {
-    environment: 'happy-dom',
-  },
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  return {
+    plugins: [
+      react(),
+      createHtmlPlugin({
+        minify: true,
+        inject: {
+          data: {
+            VITE_DEKORATOREN_URL: env.VITE_DEKORATOREN_URL,
+          },
+        },
+      }),
+    ],
+    server: {
+      port: 3000,
+    },
+    test: {
+      environment: 'happy-dom',
+    },
+  }
 })
