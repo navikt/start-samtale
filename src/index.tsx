8b8a095e877069b890186197365a743aebfef216
marcjas <marc.joseph.ablian.saputil@nav.no>
2023-02-13 10:21:03 +0100

    use mswjs in dev/gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import App from './App'
 import { createRoot } from 'react-dom/client'
 
-if (import.meta.env.DEV) {
+const useMock = import.meta.env.DEV || import.meta.env.VITE_USE_HASH_ROUTER
+
+if (useMock) {
   import('./mock')
     .then(({ default: startWorker }) => startWorker())
     .then(() => createRoot(document.getElementById('root')!!).render(<App />))
